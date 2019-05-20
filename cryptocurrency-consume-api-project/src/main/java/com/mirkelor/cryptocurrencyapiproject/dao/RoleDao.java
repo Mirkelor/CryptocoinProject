bc9ddf98e9373f56c9e8d2bf0b95dd619f4a137f
mirkelor <hasanefee@hotmail.com>
2019-05-20 18:42:01 +0300

    Added Index page

--- cryptocurrency-consume-api-project/src/main/java/com/mirkelor/cryptocurrencyapiproject/dao/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.mirkelor.cryptocurrencyapiproject.dao;
-
-import com.mirkelor.cryptocurrencyapiproject.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleDao extends JpaRepository<Role, Integer> {
-
-    Role findByRole(String role);
-}
